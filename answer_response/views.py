5f0dd13ec4a9ad3586da04c748373033711901bb
Iqrar ahnad <iqrar4006@gmail.com>
2024-03-13 20:40:20 +0530

    Added Basic Authentication

--- answer_response/views.py
+++ answer_response/views.py
@@ -3,24 +3,28 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.http import HttpResponse
 from .serializers import ValidateQuestionSerializer
+from rest_framework.permissions import IsAuthenticated
+
 
 
 # Create your views here.
 class HomeView(APIView):
+    permission_classes = [IsAuthenticated]
     def get(self,request,format=None):
         return HttpResponse('Home page')
 
 class ValidateQuestionView(APIView):
-    def get(self,request,format=None):
+    permission_classes = [IsAuthenticated]
+    def post(self,request,format=None):
         serializer = ValidateQuestionSerializer(data=request.data)
         if serializer.is_valid():
             question_text = serializer.validated_data['question']
             options = serializer.validated_data['options']
             answer = serializer.validated_data['answer']
             if answer in options:
-                return Response({'msg':f"You have selected the correct option {answer}"})
+                return Response({"correct":True,"question":question_text,"options":options,"answer":answer})
             else:
-                return Response({'msg':f"You have selected the incorrect option {answer}"})
+                return Response({"correct":False,"question":question_text,"options":options,"answer":answer})
         return Response(serializer.errors, status=400)
 
 
